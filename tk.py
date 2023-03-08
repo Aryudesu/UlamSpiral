fa1fc94436b8c1258e942fc186540ffdf32468d0
Aryudesu <suwako_a-u-@hotmail.co.jp>
2023-03-08 15:09:31 +0900

    若干のコード整理

--- tk.py
+++ tk.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 
 
-def make_window(HW, m=2):
+def make_window(hw, cell_size=2):
     result = tk.Tk()
     result.title(u"Ulam Spiral")
-    result.geometry(f"{HW*m}x{HW*m}")
+    result.geometry(f"{hw*cell_size}x{hw*cell_size}")
     return result
 
 
-def draw(win, data, HW, m):
+def draw(win, data, hw, cell_size=2):
     cv = tk.Canvas(win, bg="white")
     cv.pack(fill=tk.BOTH, expand=True)
-    for i_y in range(HW + 2):
-        for i_x in range(HW + 2):
+    for i_y in range(hw + 2):
+        for i_x in range(hw + 2):
             if data[i_y][i_x]:
                 cv.create_rectangle(
-                    i_x * m, i_y * m, (i_x + 1) * m, (i_y + 1) * m, fill="#000000")
+                    i_x * cell_size, i_y * cell_size, (i_x + 1) * cell_size, (i_y + 1) * cell_size, fill="#000000")
--- ulam.py
+++ ulam.py
@@ -17,47 +17,39 @@ def calc_prime(N):
     return primes
 
 
-def make_data(HW, primes):
+def make_data(hw, primes):
     """画面描画用データ作成"""
-    result = [[None for _ in range(HW + 2)] for _ in range(HW + 2)]
+    result = [[None for _ in range(hw + 2)] for _ in range(hw + 2)]
     # 周囲をFalseで囲む
-    for idx in range(HW + 2):
-        result[0][idx] = False
-        result[HW + 1][idx] = False
-        result[idx][0] = False
-        result[idx][HW + 1] = False
-    tmp = HW * HW
-    x = 1
-    y = HW
-    dx = 0
-    dy = -1
+    for idx in range(hw + 2):
+        result[0][idx] = result[hw+1][idx] = False
+        result[idx][0] = result[idx][hw + 1] = False
+    x, y = 1, hw
+    dx, dy = 0, -1
     # 外周から値を埋めていく
-    while tmp > 0:
-        result[y][x] = tmp in primes
+    for count in range(hw * hw, 0, -1):
+        result[y][x] = count in primes
         if result[y + dy][x + dx] is not None:
-            if dy == -1:
+            if abs(dy) == 1:
+                dx = -dy
                 dy = 0
-                dx = 1
-            elif dy == 1:
-                dy = 0
-                dx = -1
-            elif dx == -1:
-                dy = -1
-                dx = 0
-            elif dx == 1:
-                dy = 1
+            elif abs(dx) == 1:
+                dy = dx
                 dx = 0
         x += dx
         y += dy
-        tmp -= 1
     return result
 
 
-N = 100
-HW = N * 2 + 1
-m = 4
-primes = calc_prime(HW * HW)
-data = make_data(HW, primes)
-win = tk.make_window(HW+2, m)
-tk.draw(win, data, HW, m)
-win.mainloop()
+def main(N):
+    """本体"""
+    hw = N * 2 + 1
+    cell_size = 4
+    primes = calc_prime(hw * hw)
+    data = make_data(hw, primes)
+    win = tk.make_window(hw+2, cell_size)
+    tk.draw(win, data, hw, cell_size)
+    win.mainloop()
+
+
+main(100)
